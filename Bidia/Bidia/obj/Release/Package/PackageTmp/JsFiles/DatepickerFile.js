6fba1f612fb3d4b92f31ca4790c7f126371a8b64
arif1613 <arif1613@yahoo.com>
2016-02-29 00:37:50 +0100

    Deployed to azure with azure database and design changed

---
+++ Bidia/Bidia/obj/Release/Package/PackageTmp/JsFiles/DatepickerFile.js
@@ -0,0 +1,17 @@
+﻿
+// When the document is ready
+$(document).ready(function () {
+
+
+		$(".datepicker").datepicker({
+			//startDate: '-3y',
+			todayHighlight: true
+		});
+
+	$("#Dob").datepicker({
+		todayHighlight: true
+	});
+
+
+
+});
