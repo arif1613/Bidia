73548fdc3c97a0d28cf62e7d4d92098a8abeb8a9
arif1613 <arif1613@yahoo.com>
2016-02-28 01:19:07 +0100

    Item type model changed

---
+++ Bidia/Bidia/Controllers/ItemModelsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Bidia.Models.BidiaModels;
+
+namespace Bidia.Controllers
+{
+    public class ItemModelsController : Controller
+    {
+        private BidiaDB db = new BidiaDB();
+
+        // GET: ItemModels
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Items.ToListAsync());
+        }
+
+        // GET: ItemModels/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemModel itemModel = await db.Items.FindAsync(id);
+            if (itemModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemModel);
+        }
+
+        // GET: ItemModels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ItemModels/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,ItemDescription,ItemProductType,ItemOriantation,ItemDatetime,ItemPrice,IsItemAvailable,DeliveryTime")] ItemModel itemModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Items.Add(itemModel);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(itemModel);
+        }
+
+        // GET: ItemModels/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemModel itemModel = await db.Items.FindAsync(id);
+            if (itemModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemModel);
+        }
+
+        // POST: ItemModels/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,ItemDescription,ItemProductType,ItemOriantation,ItemDatetime,ItemPrice,IsItemAvailable,DeliveryTime")] ItemModel itemModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(itemModel).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(itemModel);
+        }
+
+        // GET: ItemModels/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemModel itemModel = await db.Items.FindAsync(id);
+            if (itemModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemModel);
+        }
+
+        // POST: ItemModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            ItemModel itemModel = await db.Items.FindAsync(id);
+            db.Items.Remove(itemModel);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Bidia/Bidia/Models/BidiaModels/ItemModel.cs
+++ Bidia/Bidia/Models/BidiaModels/ItemModel.cs
@@ -15,9 +15,13 @@ namespace Bidia.Models.BidiaModels
 		public string ItemDescription { get; set; }
 		[DataType(DataType.Text)]
 		public string ItemProductType { get; set; }
+		[DataType(DataType.Text)]
+		public string ItemOriantation { get; set; }
+		[DataType(DataType.DateTime)]
+		public DateTime ItemDatetime { get; set; }
 		[Required]
 		public double ItemPrice { get; set; }
-		[Required]
+		public bool IsHomePage { get; set; }
 		public bool IsItemAvailable { get; set; }
 		public string DeliveryTime { get; set; }
 	}
